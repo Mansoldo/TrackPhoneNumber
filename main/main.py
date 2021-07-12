d0dc054b6ac90fae1ea2e3953f6a5038e3b244b9
Mansoldo <dmansoldo@gmail.com>
2021-07-12 08:13:49 -0300

    Adjusting the comments

--- main/main.py
+++ main/main.py
@@ -4,28 +4,27 @@ import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier
 
-
-# Ajuste o formato do telefone
 telefone = '+5511959459690'
 
+# Adjusting the phone number according to the region
 telefone_ajustado = phonenumbers.parse(telefone, 'pt-br')
 print(telefone_ajustado)
 
-# Descobrir a localização do telefone
+# Discovering the number location
 local = geocoder.description_for_number(telefone_ajustado, 'pt-br')
 print(local)
 
-# Formatando o número
+# Formating the number
 telefone_formulario = '+5511959459690'
 telefone_formulario_ajustado = phonenumbers.parse(telefone_formulario, 'pt-br')
 telefone_formato = phonenumbers.format_number(telefone_formulario_ajustado, phonenumbers.PhoneNumberFormat.NATIONAL)
 print(telefone_formato)
 
-# Operadora do telefone
+# Telephone provider
 operadora = carrier.name_for_number(telefone_ajustado, 'pt-br')
 print(operadora)
 
-# Retirar telefone do texto
+# Removing a phone number from a text
 corpo_email = '' \
               'Prezados, ' \
               'Quando tiverem uma resposta da proposta, favor entrar em contato. ' \
